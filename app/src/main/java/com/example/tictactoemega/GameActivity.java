f8e3be9509f305ea80fd65cc051df8b9b8abf08d
Hriday Keswani <hridaykeswani@gmail.com>
2021-07-30 17:14:03 +0530

    added onclick functionality to eac button and made the ui colour blind friendly

--- app/src/main/java/com/example/tictactoemega/GameActivity.java
+++ app/src/main/java/com/example/tictactoemega/GameActivity.java
@@ -2,13 +2,34 @@ package com.example.tictactoemega;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
+
+    private int count=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void action(View view){
+        String name = getResources().getResourceName(view.getId());
+        Toast.makeText(this, name.substring(name.length()-2,name.length()),Toast.LENGTH_SHORT).show();
+
+        Button press = findViewById(view.getId());
+
+        if(count%2==0){
+            press.setText("X");
+        }else{
+            press.setText("O");
+        }
+
+        count++;
     }
 }
